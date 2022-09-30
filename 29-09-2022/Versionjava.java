5cd241e969dbd46a39e4eafbafc0063b6e574924
mishra-5667 <113969617+mishra-5667@users.noreply.github.com>
2022-09-30 17:31:22 +0530

    Add files via upload

---
+++ 29-09-2022/Versionjava.java
@@ -0,0 +1,10 @@
+package corejava;
+//declaring class 
+public class Versionjava {
+//calling main method
+	public static void main(String[] args) {
+	
+System.out.println("version of java is:"+System.getProperty("java.version"));//printing version of java
+	}// end of main
+
+}//end of class 
