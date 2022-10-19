47d2e32c37c4c9b32401a295930cdf9dda4e8671
mishra-5667 <113969617+mishra-5667@users.noreply.github.com>
2022-10-19 16:00:16 +0530

    Add files via upload

---
+++ corejava/src/corejava/Array.java
@@ -0,0 +1,19 @@
+package corejava;
+
+public class Array {
+
+	public static void main(String[] args) {
+	//declaring and  instantiating the array
+		int a[]= {27,0,4,7,1};
+		//initialization
+		a[0]=12;
+		a[1]=13;
+		a[2]=15;
+		//traversing the array
+		for(int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+		}
+
+	}
+
+}
