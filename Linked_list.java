70dbcc1361ac061d94e057c5d7d88ca15ff2228f
mishra-5667 <113969617+mishra-5667@users.noreply.github.com>
2022-11-02 21:58:54 +0530

    Add files via upload

---
+++ Linked_list.java
@@ -0,0 +1,27 @@
+/*
+ *linked list implements the collection interface and it also stores duplicate data 
+ */
+package Java.ac;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Linked_list {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> ob=new LinkedList<Integer>();//creating linkedlist
+		//adding object in linkedlist
+		ob.add(4);
+		ob.add(2);
+		ob.add(5);
+		//System.out.println(ob);
+		Iterator a=ob.iterator();
+		while(a.hasNext())
+		{
+			System.out.println(a.next());
+		}
+	}
+	
+}
+
+
