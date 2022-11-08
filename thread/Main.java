b7f3b00874c1fc9c37754ebe60ef888e20ec224e
mishra-5667 <113969617+mishra-5667@users.noreply.github.com>
2022-11-08 21:24:10 +0530

    Update Main.java

--- thread/Main.java
+++ thread/Main.java
@@ -1,9 +1,21 @@
 package thread;
-
-public class Main {
+/*
+ * If the class extends the Thread class,
+ * the thread can be run by creating an instance of the class
+ * and call its start() method.
+ * and overriding its run() method.
+  */
+public class Main extends Thread {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			Main m=new Main();
+		m.start();
+		System.out.println("The thread is created with name:" +m.getName());
+	}
+	public void run()
+	{
+		System.out.println("The code is inside the thread");
+	}
 
 	}
 
